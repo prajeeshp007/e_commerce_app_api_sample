8f21782793d92a7af538461edd095fb3606d7e39
prajeeshp007 <prajeeshprmod1@gmail.com>
2024-08-29 19:43:10 +0530

    category changed

--- lib/controller/home_screen_controller.dart
+++ lib/controller/home_screen_controller.dart
@@ -1,45 +1,65 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:e_commerce_app_api_sample/model/home_screen_model/get_all_products_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class HomeScreenController with ChangeNotifier {
-  /// evda all nn kodtath categoriesill All enn ullla option kanikkan hann
-  List categorieslist = ["ALL"];
-  List<AllProductsDataResModel> products = [];
-  int selectedcategorycolorindex = 0;
+  /// variable used for categoriyil lodaing kodkann
+  bool categoryisloading = false;
 
-  Future<void> getAllproductdetails() async {
-    final url = Uri.parse('https://fakestoreapi.com/products');
+  /// variable for allproducts loading
+  bool productsloading = false;
+
+  /// evda all nn kodtath categoriesill All enn ullla option kanikkan hann
+  /// variable for used to add #All in category
+  List categorieslist = ["ALL"];
+
+  /// variable used for all productsinta list vilikan
+  List<AllProductsDataResModel> products = [];
+
+  /// section for get all products
+  Future<void> getAllproductdetails({String category = ''}) async {
+    productsloading = true;
+    notifyListeners();
+    final allurl = Uri.parse('https://fakestoreapi.com/products');
+    final categoryurl =
+        Uri.parse('https://fakestoreapi.com/products/category/$category');
+
+    final url = category.isEmpty ? allurl : categoryurl;
     var responsedata = await http.get(url);
     print(responsedata.statusCode);
     print(responsedata.body);
     if (responsedata.statusCode == 200) {
       products = allProductsDataResModelFromJson(responsedata.body);
     }
+    productsloading = false;
     notifyListeners();
   }
 
-  /// to get category datas
+  /// section  to get category datas
   Future<void> getcategory() async {
+    categoryisloading = true;
+    notifyListeners();
     try {
       final url = Uri.parse('https://fakestoreapi.com/products/categories');
       var res = await http.get(url);
 
       if (res.statusCode == 200) {
-        /// . add all vilichath all enn ullath categoriesil aadhym all kanikan hann
+        /// . add all vilichath all enn ullath categoriesil aadhym all kanikan hann adhin evda category list assign cheyyunathin pakaraam add all vech ALL enn ulla category add chyth
         categorieslist.addAll(jsonDecode(res.body));
         print(categorieslist);
       }
     } catch (e) {
       print(e);
     }
+    categoryisloading = false;
     notifyListeners();
   }
 
-  oncategory(int index) {
-    selectedcategorycolorindex = index;
-    notifyListeners();
-  }
+  // oncategory(int index) {
+  //   selectedcategorycolorindex = index;
+  //   notifyListeners();
+  // }
 }
--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -66,90 +66,128 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Consumer<HomeScreenController>(
-            builder: (context, homeprovider, child) => Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Categories',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    DropdownButton(
-                      /// evda drop down value empty hanalll all enn ullath aaadhym kanikmm
-                      value: dropvalue ?? 'ALL',
-                      items: List.generate(
-                        /// lengt kodukan ulla code
-                        homeprovider.categorieslist.length,
-                        (index) => DropdownMenuItem(
-                            value: homeprovider.categorieslist[index],
-                            child: Text(homeprovider.categorieslist[index]
-                                .toUpperCase())),
-                      ),
-                      onChanged: (value) {
-                        dropvalue = value as String?;
-                        setState(() {});
-                      },
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: GridView.builder(
-                      itemCount: homeprovider.products.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          mainAxisExtent: 400,
-                          crossAxisSpacing: 30,
-                          crossAxisCount: 2),
-                      itemBuilder: (context, index) => Container(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Container(
-                                  height: 200,
-                                  decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          fit: BoxFit.cover,
-                                          image: NetworkImage(homeprovider
-                                              .products[index].image
-                                              .toString())),
-                                      shape: BoxShape.circle),
-                                ),
-                                Text(
-                                  homeprovider.products[index].title.toString(),
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 15),
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Icon(
-                                      Icons.currency_bitcoin,
-                                      color: Colors.black,
-                                      size: 30,
-                                    ),
-                                    Text(
-                                      homeprovider.products[index].price
-                                          .toString(),
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 25,
-                                          fontWeight: FontWeight.w900),
-                                    ),
-                                  ],
-                                ),
-                              ],
+            builder: (context, homeprovider, child) => homeprovider
+                    .categoryisloading
+
+                /// for category load cheyyumboo loading kanikan ee loading kanicha shesham hann category section kanikku
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Categories',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          DropdownButton(
+                            /// evda drop down value empty hanalll all enn ullath aaadhym kanikmm
+                            value: dropvalue ?? 'ALL',
+                            items: List.generate(
+                              /// lengt kodukan ulla code
+                              homeprovider.categorieslist.length,
+                              (index) => DropdownMenuItem(
+                                  value: homeprovider.categorieslist[index],
+                                  child: Text(homeprovider.categorieslist[index]
+                                      .toUpperCase())),
                             ),
-                          )),
-                )
-              ],
-            ),
+                            onChanged: (value) async {
+                              await context
+                                  .read<HomeScreenController>()
+                                  .getAllproductdetails(
+                                      category: value.toString());
+                              dropvalue = value as String?;
+                              setState(() {});
+                            },
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: homeprovider.productsloading
+
+                            /// for product load cheyyunna tymil lodaing indicator kanikan category load chyth kazhinn product kanikumbo loaing varan
+                            ? Center(
+                                child: CircularProgressIndicator(),
+                              )
+                            : GridView.builder(
+                                itemCount: homeprovider.products.length,
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                        mainAxisExtent: 350,
+                                        crossAxisSpacing: 30,
+                                        crossAxisCount: 2),
+                                itemBuilder: (context, index) => Container(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                            height: 200,
+                                            decoration: BoxDecoration(
+                                                image: DecorationImage(
+                                                    fit: BoxFit.cover,
+                                                    image: NetworkImage(
+                                                        homeprovider
+                                                            .products[index]
+                                                            .image
+                                                            .toString())),
+                                                shape: BoxShape.circle),
+                                          ),
+                                          Text(
+                                            maxLines: 1,
+                                            homeprovider.products[index].title
+                                                .toString(),
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 15),
+                                          ),
+                                          Text(
+                                            'Rating:-${homeprovider.products[index].rating!.rate.toString()}',
+                                            style: TextStyle(
+                                                color: Colors.red,
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w900),
+                                          ),
+                                          Text(
+                                            'Only:-${homeprovider.products[index].rating!.count.toString()} Left',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w900),
+                                          ),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              Icon(
+                                                Icons.currency_bitcoin,
+                                                color: Colors.black,
+                                                size: 30,
+                                              ),
+                                              Text(
+                                                homeprovider
+                                                    .products[index].price
+                                                    .toString(),
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 25,
+                                                    fontWeight:
+                                                        FontWeight.w900),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    )),
+                      )
+                    ],
+                  ),
           ),
         ),
       ),
